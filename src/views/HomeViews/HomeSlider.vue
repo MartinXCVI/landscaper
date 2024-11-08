a3efaa5ad33f93248b8dc9c517b3aa39bbbe3528
MartinXCVI <mdzg96@gmail.com>
2024-11-07 22:31:17 -0300

    Progress in slider section

--- src/views/HomeViews/HomeSlider.vue
+++ src/views/HomeViews/HomeSlider.vue
@@ -1,17 +1,26 @@
 
 <template>
-  <section class="slider-container">
-    <div class="slider">
-      <div class="front"></div>
-      <div class="back"></div>
-      <div class="left"></div>
-      <div class="right"></div>
-    </div>
-    <br/>
-    <div class="slider-buttons">
-      <button class="slider-btn" @click="leftSide()">Left</button>
-      <button class="slider-btn" @click="rightSide()">Right</button>
-    </div>
+  <section class="slider-section">
+    <article class="slider-article">
+      <h2 class="subtitle capital">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
+      <div class="line"></div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita quibusdam laborum repellat aspernatur in hic vel rerum facere sunt sit.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam provident dolore ex exercitationem illum! Placeat laudantium excepturi laborum cupiditate inventore quidem pariatur molestiae in et!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quas facere laudantium natus dolorum obcaecati inventore perspiciatis recusandae! Temporibus delectus, exercitationem saepe quas est iste autem accusantium esse eius ex obcaecati magnam ducimus, nulla et.</p>
+    </article>
+    <section class="slider-container">
+      <div class="slider">
+        <div class="front"></div>
+        <div class="back"></div>
+        <div class="left"></div>
+        <div class="right"></div>
+      </div>
+      <br/>
+      <div class="slider-buttons">
+        <button class="slider-btn" @click="leftSide()">Left</button>
+        <button class="slider-btn" @click="rightSide()">Right</button>
+      </div>
+    </section>
   </section>
 </template>
 
@@ -41,9 +50,33 @@
 
 <style>
 
-.slider-container {
+.slider-section {
   width: 100%;
-  min-height: 90vh;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: linear-gradient(145deg, rgba(0,119,119,0.6) 0%, rgba(239,239,239,1) 20%, rgba(211,211,211,1) 40%, rgba(211,211,211,1) 60%, rgba(239,239,239,1) 80%, rgba(0,119,119,0.6) 100%);
+  padding: 3rem 2rem;
+  gap: 1.5rem;
+}
+
+.slider-article {
+  width: 45%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 0.75rem;
+  .line {
+    width: 100%;
+    border-bottom: 3px solid #007777;
+    margin-bottom: 1rem;
+  }
+}
+
+.slider-container {
+  width: 45%;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -77,7 +110,7 @@
   height: 400px;
   position: absolute;
   box-shadow: 0 0 5px #ccc, inset 0 0 10px #ddd;
-  transform: translateY(50px);
+  /* transform: translateY(50px); */
 }
 
 .front {
@@ -155,12 +188,20 @@
   text-shadow: 0 0 0.25rem darkgray;
   box-shadow: 0 0 0.25rem darkgray;
   font-size: 1.25rem;
-  background-color: rgb(101, 214, 101);
+  background-color: #007777;
   color: white;
-  transition: 100ms;
+  transition: 300ms;
+  &:hover {
+    background-color: #65d1a0;
+    color: #007777;
+  }
 }
 
 @media all and (max-width: 375px) {
+
+  .slider-article {
+    margin-bottom: 4.5rem;
+  }
 
   .slider {
     width: 200px;
@@ -190,6 +231,10 @@
 }
 
 @media all and (min-width: 376px) and (max-width: 767px) {
+
+  .slider-article {
+    margin-bottom: 2rem;
+  }
 
   .slider {
     width: 300px;
@@ -224,6 +269,23 @@
   .slider {
     margin-bottom: 3rem;
   }
+}
+
+@media all and (max-width: 991px) {
+
+  .slider-section {
+    flex-direction: column;
+    gap: 2rem;
+  }
+
+  .slider-article {
+    width: 100%;
+  }
+
+  .slider-container {
+    width: 100%;
+  }
+
 }
 
 </style>
