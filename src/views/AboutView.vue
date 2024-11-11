a30a0ae1c3538598a080bfd676e77409e24b9bdf
MartinXCVI <mdzg96@gmail.com>
2024-11-10 23:14:41 -0300

    Added introduction for about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,6 +2,7 @@
   import HeroHeader from '@/components/HeroHeader.vue';
   import Header from '@/components/Header.vue';
   import Navbar from '@/components/Navbar.vue';
+  import AboutIntro from './AboutViews/AboutIntro.vue';
   import Footer from '@/components/Footer.vue';
 </script>
 
@@ -12,6 +13,6 @@
     </Header>
     <h1 class="header-title">About Us</h1>
   </HeroHeader>
-  
+  <AboutIntro />
   <Footer />
 </template>
---
+++ src/views/AboutViews/AboutIntro.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="about-intro-section">
+    <section class="about-intro">
+      <h2 class="capital subtitle">beautifying landscapes since 1996</h2>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut dolorem dignissimos quo laboriosam, modi perferendis quibusdam! Amet repudiandae nesciunt quis facere cum quasi facilis tempora.</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut dolorem dignissimos quo laboriosam, modi perferendis quibusdam! Amet repudiandae nesciunt quis facere cum quasi facilis tempora.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum repellendus similique maiores expedita facilis accusantium illo voluptatibus ut porro repudiandae nostrum laudantium harum laborum doloribus, in dignissimos, aperiam commodi quasi magnam consequatur corporis alias vitae.</p>
+      <a class="button btn-first caps" href="/projects">Our works</a>
+    </section>
+
+    <section class="about-figures-section">
+      <div class="figures-div">
+        <figure class="about-figure">
+          <img src="/images/about-vertical-1.jpg" alt="">
+        </figure>
+        <figure class="about-figure">
+          <img src="/images/about-horizontal-1.jpg" alt="">
+        </figure>
+      </div>
+      <div class="figures-div">
+        <figure class="about-figure">
+          <img src="/images/about-horizontal-2.jpg" alt="">
+        </figure>
+        <figure class="about-figure">
+          <img src="/images/about-vertical-2.jpg" alt="">
+        </figure>
+      </div>
+    </section>
+
+  </section>
+</template>
