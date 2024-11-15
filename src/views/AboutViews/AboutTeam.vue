ef57ba41f3ce7917e822536af60c7de0e5519b8f
MartinXCVI <mdzg96@gmail.com>
2024-11-15 12:31:05 -0300

    Small modifications

--- src/views/AboutViews/AboutTeam.vue
+++ src/views/AboutViews/AboutTeam.vue
@@ -6,21 +6,27 @@
         <img src="/images/team-1.jpg" alt="">
         <div class="member-info">
           <h4 class="capital">Surname, name</h4>
-          <i class="fa-brands fa-facebook"></i>
+          <a href="https://www.facebook.com/" target="_blank">
+            <i class="fa-brands fa-facebook"></i>
+          </a>
         </div>
       </figure>
       <figure class="member-fig" data-aos="fade-up" data-aos-duration="2000">
         <img src="/images/team-2.jpg" alt="">
         <div class="member-info">
           <h4 class="capital">Surname, name</h4>
-          <i class="fa-brands fa-facebook"></i>
+          <a href="https://www.facebook.com/" target="_blank">
+            <i class="fa-brands fa-facebook"></i>
+          </a>
         </div>
       </figure>
       <figure class="member-fig" data-aos="fade-down" data-aos-duration="2000">
         <img src="/images/team-3.jpg" alt="">
         <div class="member-info">
           <h4 class="capital">Surname, name</h4>
-          <i class="fa-brands fa-facebook"></i>
+          <a href="https://www.facebook.com/" target="_blank">
+            <i class="fa-brands fa-facebook"></i>
+          </a>
         </div>
       </figure>
     </div>
